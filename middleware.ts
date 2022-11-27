fa980fc05fbbf9c51b760dafd04fd48e377611e8
marianochavez <amarianochavez@gmail.com>
2022-11-27 16:37:16 -0300

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+    const session = await getToken({ req });
+
+    if (!session) {
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+}
+
+export const config = {
+    matcher: ["/resources"]
+}
